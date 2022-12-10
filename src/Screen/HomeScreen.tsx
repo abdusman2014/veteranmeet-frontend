a23e462af2f577e08b787faaf5684ea81a9a6d55
abdusman2002 <abdullah@connectia.io>
2022-12-10 18:40:57 +0500

    screen and component folder

---
+++ src/Screen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Screen from '../Components/Screen';
+
+function HomeScreen() {
+    return (
+        <div>
+        <Screen></Screen>
+        </div>
+    );
+}
+
+export default HomeScreen;
